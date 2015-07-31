e815599e7da82cd6e3143d2c0f2324272c0cb555
PurnankJain <purnank_jain@yahoo.com>
2015-07-31 10:11:37 +0530

    [Purnank] Rover can Turn right.

--- src/com/marsRover/three/Rover.java
+++ src/com/marsRover/three/Rover.java
@@ -5,15 +5,30 @@ public class Rover {
     private int x;
     private int y;
     private int direction;
+    private String sDirection;
 
     public Rover(int x, int y, int direction) {
         this.x = x;
         this.y = y;
         this.direction = direction;
+        setSDirection();
+    }
+
+    private void setSDirection() {
+        switch (direction) {
+            case 0 : sDirection = "N";
+                break;
+            case 1 : sDirection = "W";
+                break;
+            case 2 : sDirection = "S";
+                break;
+            case 3 : sDirection = "E";
+                break;
+        }
     }
 
     private String stateToString() {
-        return String.format("%d, %d, %s", x, y, "N");
+        return String.format("%d, %d, %s", x, y, sDirection);
     }
 
     public String move() {
@@ -27,6 +42,12 @@ public class Rover {
             case 3 : x++;
                 break;
         }
+        return stateToString();
+    }
+
+    public String turnRight() {
+        direction--;
+        setSDirection();
         return stateToString();
     }
 }
--- test/com/marsRover/three/RoverTest.java
+++ test/com/marsRover/three/RoverTest.java
@@ -18,20 +18,27 @@ public class RoverTest {
     public void shouldMoveDownWhenSouth() {
         Rover rover = new Rover(0, 1, 2);
 
-        assertEquals("0, 0, N", rover.move());
+        assertEquals("0, 0, S", rover.move());
     }
 
     @Test
     public void shouldMoveLeftWhenWest() {
         Rover rover = new Rover(1, 1, 1);
 
-        assertEquals("0, 1, N", rover.move());
+        assertEquals("0, 1, W", rover.move());
     }
 
     @Test
-    public void shouldMoveRightWhenWest() {
+    public void shouldMoveRightWhenEast() {
         Rover rover = new Rover(1, 1, 3);
 
-        assertEquals("2, 1, N", rover.move());
+        assertEquals("2, 1, E", rover.move());
+    }
+
+    @Test
+    public void shouldTurnRightOnRight() {
+        Rover rover = new Rover(1, 1, 1);
+
+        assertEquals("1, 1, N", rover.turnRight());
     }
 }
